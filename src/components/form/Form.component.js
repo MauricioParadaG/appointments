1e3829f9b80797518951497c188695dfc2dd89bc
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-06-02 14:57:45 +0200

    Creating a form with validation of all filds are required. With hooks, error state message, and Css
    Creating a form with validation of all filds are required. With hooks, error state message, and Css

--- src/components/form/Form.component.js
+++ src/components/form/Form.component.js
@@ -1,18 +1,70 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const FormComponent = () => {
+
+    const [appointment, setAppointmentState] = useState(
+        {
+           client:'',
+           adviser:'',
+           date:'',
+           hour:'',
+           details:''
+        }
+      );
+
+    const [error, setErrorState] = useState(false);
+
+
+    const onChangeForm = event => {
+        setAppointmentState({
+            ...appointment,
+            [event.target.name]: event.target.value
+        });
+    } 
+
+    //const {client, advisor, date, hour, details} = appointment;
+
+    const onSubmit = event => {
+        event.preventDefault();
+        // validation
+        if (appointment.client.trim() ==='' ||          appointment.adviser.trim() ==='' ||
+        appointment.date.trim() ==='' ||
+        appointment.hour.trim() ==='' ||
+        appointment.details.trim() ==='' )
+        //console.log("one or more filds are empty");
+            setErrorState(true);
+            return;
+/*
+        const newAppointnment = {
+            client: state.userSelected,
+            adviser: state.title,
+            date: state.content,
+            hour: state.date,
+            details:''
+        };
+*/
+    }
+
+    
+
     return (
         <Fragment>
             <h2>Create New Appointment</h2>
-            <form onSubmit={1}>
+            {/** if - error message with .css*/}
+            {error ?
+            <p className="alert-error">All filds are required</p>
+            : null
+            }
+
+            <form onSubmit={onSubmit}>
             {/** Client */}
                 <label>Client Name</label>
                  <input type="text"
                  name= "client"
                  placeholder="Client Name"
                  className="u-full-width"
-                 onChange={"this.onChangeName"}
-                 value={"this.state.name"}
+                 onChange={onChangeForm}
+                 value={appointment.client}
                 />
             {/** Adviser */}
                 <label>Adviser Name</label>
@@ -20,16 +72,16 @@ const FormComponent = () => {
                  name= "adviser"
                  placeholder="Adviser Name"
                  className="u-full-width"
-                 onChange={"this.onChangeName"}
-                 value={"this.state.name"}
+                 onChange={onChangeForm}
+                 value={appointment.adviser}
                 />
             {/** Date */}
                 <label>Date</label>
                  <input type="date"
                  name= "date"
                  className="u-full-width"
-                 onChange={"this.onChangeName"}
-                /*value={"this.state.name"}*/
+                 onChange={onChangeForm}
+                 value={appointment.date}
                 />
 
             {/** Hour */}
@@ -37,19 +89,24 @@ const FormComponent = () => {
                  <input type="time"
                  name= "hour"
                  className="u-full-width"
-                 onChange={"this.onChangeName"}
-               /*  value={"this.state.name"}*/
+                 onChange={onChangeForm}
+                 value={appointment.hour}
                 />
 
             {/** Details */}
                 <label>Details</label>
-                 <input type="text"
+                <textarea type="text"
                  name= "details"
                  placeholder="More Information"
                  className="u-full-width"
-                 onChange={"this.onChangeName"}
-                 value={"this.state.name"}
-                />     
+                 onChange={onChangeForm}
+                 value={appointment.details}></textarea>
+
+            {/** Button */}
+                <button type="submit"
+                className="u-full-width button-primary">
+                    Create
+                </button>                 
 
             </form>
 
