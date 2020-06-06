aafa908fdcca1bebbd4925c2a96310f1abe3c490
MauricioParadaG <62025899+MauricioParadaG@users.noreply.github.com>
2020-06-06 14:34:36 +0200

    It is just an space, project completed and up (deployed) in website
    https://eager-mcclintock-a7aa2d.netlify.app/

--- src/components/header/Header.component.js
+++ src/components/header/Header.component.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const HeaderComponent = props => (
 <h3>
-    {props.userFaztState}Appointment Administrator ({props.appointmentsState.filter(appointment => !appointment.done).length} to attent)
+    {props.userFaztState}Appointment Administrator ( {props.appointmentsState.filter(appointment => !appointment.done).length} to attent)
 </h3>
 )
 
